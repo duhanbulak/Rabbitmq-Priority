1416800d93caf9cccf93dd4b4b9991c7f0c66d23
Duhan Taha BULAK <duhanbulak@gmail.com>
2020-11-10 13:49:40 +0300

    Add project files

---
+++ Rabbitmq-Priority.Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { Uri = new Uri(Environment.GetEnvironmentVariable("RABBITMQ_URI")) };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "PriortyQueue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: new Dictionary<string, object>()
+                        {
+                            {"x-max-priority", 10},
+                        });
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+
+                    channel.BasicConsume(queue: "PriortyQueue",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine($" [x] Received {Encoding.UTF8.GetString(e.Body.ToArray())}");
+        }
+    }
+}
---
+++ Rabbitmq-Priority.Producer/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { Uri = new Uri(Environment.GetEnvironmentVariable("RABBITMQ_URI")) };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    Random r = new Random();
+
+                    channel.QueueDeclare(queue: "PriortyQueue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: new Dictionary<string, object>()
+                        {
+                            { "x-max-priority", 10 },
+                        });
+
+                    IBasicProperties props = channel.CreateBasicProperties();
+
+                    for (int i = 0; i < 30; i++)
+                    {
+                        int priority = r.Next(10);
+                        props.Priority = (byte)priority;
+
+                        string message = $"Hello World! {i} / Priorty: {priority}";
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "",
+                            routingKey: "PriortyQueue",
+                            basicProperties: props,
+                            body: body);
+
+                        Console.WriteLine($" [x] Sent {message}");
+                    }
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
